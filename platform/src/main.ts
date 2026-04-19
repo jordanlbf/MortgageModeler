020045c7d76b38131ab929ce6e13f754f32f6a1a
Jordan Brewis Foulger <jordan.lb.foulger@gmail.com>
2026-04-19 10:51:21 +1000

    feat(platform): add PrismaService and PrismaModule

--- platform/src/main.ts
+++ platform/src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableShutdownHooks();
+
   app.enableCors({
     origin: process.env.FRONTEND_ORIGIN ?? 'http://localhost:3000',
     credentials: true,
